c4ee38578fdf13013e64e6ea2d47fdd1a260cda9
luisvag <luisfranvag@gmail.com>
2023-12-14 13:12:11 -0400

    lol

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 import bs4
 import requests
 
-resultado = requests.get('https://escueladirecta-blog.blogspot.com/2023/05/configurando-la-impresion-perfecta-de.html')
+resultado = requests.get('https://www.leagueoflegends.com/es-es/news/dev/dev-league-s-business-model-in-2023/')
 
 #bs4(beautifulsoup) sirve para navegar entre todo el codigo y encontrar los elementos que buscamos
 #pide dos parametros 1(el string a transformar en elemento bs4)2(motor para hacer el parcing)
