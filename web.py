da346a1227d4d486ea85007624437554028c2551
luisvag <luisfranvag@gmail.com>
2023-12-14 13:28:53 -0400

    extraer imagenes

--- web.py
+++ web.py
@@ -1,10 +1,21 @@
 import bs4
 import requests
 
-resultado = requests.get('https://www.leagueoflegends.com/es-es/news/dev/dev-league-s-business-model-in-2023/')
+resultado = requests.get('https://www.leagueoflegends.com/es-es/news/community/drawrenekton-art-competition-winners-announced/')
 
 #bs4(beautifulsoup) sirve para navegar entre todo el codigo y encontrar los elementos que buscamos
 #pide dos parametros 1(el string a transformar en elemento bs4)2(motor para hacer el parcing)
 beautysoup = bs4.BeautifulSoup(resultado.text, 'lxml')
 
-print(beautysoup.select('title')[0].getText())
+#print(beautysoup.select('title')[0].getText())
+
+imagenes = beautysoup.select('img')[0]['src']
+print(imagenes)
+
+imgen_renek = requests.get(imagenes)
+
+#wb = writewrite binary
+d = open('renek_img.jpg', 'wb')
+d.write(imgen_renek.content)
+d.close()
+
